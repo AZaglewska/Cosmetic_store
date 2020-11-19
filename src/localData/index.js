7af5f933d4e9adf2ee60cb21905702f24ea892ac
Anna Żaglewska <web.annazaglewska@gmail.com>
2020-11-19 11:58:38 +0100

    adding basic setup, assets, localData and routes for routing

---
+++ src/localData/index.js
@@ -0,0 +1,53 @@
+import image1 from "../assets/images/pexels-cottonbro-4046316.jpg";
+import image2 from "../assets/images/pexels-dana-tentis-725998.jpg";
+import image3 from "../assets/images/pexels-karolina-grabowska-4041391.jpg";
+import image4 from "../assets/images/pexels-karolina-grabowska-4041392.jpg";
+import image5 from "../assets/images/pexels-karolina-grabowska-4202326.jpg";
+
+export const localData = [
+  {
+    productName: "Żel",
+    productPrice: 29,
+    productQuantity: 1,
+    productCategory: "żele",
+    productImage: image1,
+    productDescription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti provident, mollitia at reiciendis ullam numquam neque dolorem maiores amet eos hic in fugit expedita necessitatibus.",
+  },
+  {
+    productName: "Serum",
+    productPrice: 199,
+    productQuantity: 1,
+    productCategory: "sera",
+    productImage: image2,
+    productDescription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti provident, mollitia at reiciendis ullam numquam neque dolorem maiores amet eos hic in fugit expedita necessitatibus.",
+  },
+  {
+    productName: "Peeling",
+    productPrice: 59,
+    productQuantity: 1,
+    productCategory: "peelingi",
+    productImage: image3,
+    productDescription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti provident, mollitia at reiciendis ullam numquam neque dolorem maiores amet eos hic in fugit expedita necessitatibus.",
+  },
+  {
+    productName: "Maseczka",
+    productPrice: 9,
+    productQuantity: 1,
+    productCategory: "maseczki",
+    productImage: image4,
+    productDescription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti provident, mollitia at reiciendis ullam numquam neque dolorem maiores amet eos hic in fugit expedita necessitatibus.",
+  },
+  {
+    productName: "Krem",
+    productPrice: 299,
+    productQuantity: 1,
+    productCategory: "kremy",
+    productImage: image5,
+    productDescription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti provident, mollitia at reiciendis ullam numquam neque dolorem maiores amet eos hic in fugit expedita necessitatibus.",
+  },
+];
