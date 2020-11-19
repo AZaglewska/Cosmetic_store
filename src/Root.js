38609e411c4c8b5227ba58f5922fb6c8dbafc146
Anna Żaglewska <web.annazaglewska@gmail.com>
2020-11-19 12:03:52 +0100

    adding Root

---
+++ src/Root.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import GlobalStyle from "./GlobalStyles/GlobalStyle";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Products from "./pages/Products";
+import { routes } from "./routes";
+import MainTemplate from "./templates/MainTemplate";
+import { localData } from "./localData";
+import CosmeticStoreContext from "./context";
+
+const Root = () => {
+  const [products, setProducts] = useState([...localData]);
+
+  return (
+    <>
+      <GlobalStyle />
+      <CosmeticStoreContext.Provider value={{ products }}>
+        <BrowserRouter>
+          <MainTemplate>
+            <Switch>
+              <Route exact path={routes.home} component={Home} />
+              <Route path={routes.about} component={About} />
+              <Route path={routes.products} component={Products} />
+              <Route path={routes.contact} component={Contact} />
+            </Switch>
+          </MainTemplate>
+        </BrowserRouter>
+      </CosmeticStoreContext.Provider>
+    </>
+  );
+};
+
+export default Root;
