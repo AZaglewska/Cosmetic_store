5dd71d373fd35861244e6f2d674e5cbc46680a46
Anna Żaglewska <web.annazaglewska@gmail.com>
2020-12-02 12:58:55 +0100

    adding SliderWraper and Slider to Home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+import Slider from "../components/slider/Slider";
 
 const Home = () => {
+  const SliderWraper = styled.div`
+    width: 100%;
+    height: 80vh;
+    margin: 0;
+  `;
+
   return (
     <>
+      <SliderWraper>
+        <Slider />
+      </SliderWraper>
+
       <h1>Home</h1>
     </>
   );
