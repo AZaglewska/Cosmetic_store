1e87e3a43ee6a029c4b7999f1d7fcf8e99ac070b
Anna Żaglewska <web.annazaglewska@gmail.com>
2020-12-16 13:49:03 +0100

    adding mainTheme to gitHub

---
+++ src/GlobalStyles/mainTheme.js
@@ -0,0 +1,28 @@
+export const mainTheme = {
+  colors: {
+    lightGrey: "rgba(194, 200, 214, 0.94)",
+    smokeyGrey: "rgba(144, 149, 162, 0.8)",
+    ocean: "rgb(138, 254, 254)",
+  },
+
+  fontSize: {
+    xxs: "8px",
+    xs: "12px",
+    s: "16px",
+    m: "20px",
+    l: "24px",
+    xl: "30px",
+    xxl: "50px",
+  },
+
+  fontWeight: {
+    light: 300,
+    medium: 400,
+    bold: 500,
+  },
+
+  fontFamily: {
+    mainFont: "Montserrat",
+    secondFont: "Roboto",
+  },
+};
