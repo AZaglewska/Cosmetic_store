3bb2f95a734b9a37a66657e381ea9d47c0334d12
Anna Żaglewska <web.annazaglewska@gmail.com>
2020-12-16 14:22:05 +0100

    adding ThemeProvider and Alert component to MainTemplate

--- src/templates/MainTemplate.js
+++ src/templates/MainTemplate.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Navbar from "../components/navigations/Navbar";
-import Cart from "../components/Cart";
+import Cart from "../components/Cart/Cart";
+import { ThemeProvider } from "styled-components";
+import { mainTheme } from "../GlobalStyles/mainTheme";
+import Alert from "../components/Alert";
 
 const MainTemplate = ({ children }) => {
   return (
-    <>
+    <ThemeProvider theme={mainTheme}>
       <Navbar />
+      <Alert />
       <Cart />
       {children}
-    </>
+    </ThemeProvider>
   );
 };
 
