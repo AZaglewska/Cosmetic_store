e0003c6e5343a676bc86c90ee436334855c2d780
Anna Żaglewska <web.annazaglewska@gmail.com>
2020-12-21 12:17:03 +0100

    adding animations to GlobalStyles

---
+++ src/GlobalStyles/animations.js
@@ -0,0 +1,14 @@
+import { keyframes } from "styled-components";
+
+export const zoomIn = keyframes`
+
+0% {
+  opacity: 0;
+  -webkit-transform: scale3d(.3, .3, .3);
+  transform: scale3d(.3, .3, .3);
+  }
+  50% {
+  opacity: 1;
+  }
+
+`;
