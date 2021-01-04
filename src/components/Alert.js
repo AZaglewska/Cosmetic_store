d6c1ee092304e2f9de9a35997e66d2e54df682fe
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-01-04 12:32:14 +0100

    adding onClick function to alert component

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import MaterialAlert from "@material-ui/lab/Alert";
 import IconButton from "@material-ui/core/IconButton";
 import Collapse from "@material-ui/core/Collapse";
 import CloseIcon from "@material-ui/icons/Close";
+import CosmeticStoreContext from "../context";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,26 +17,26 @@ const useStyles = makeStyles((theme) => ({
 
 const Alert = () => {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(true);
+
+  const context = useContext(CosmeticStoreContext);
+  const { isAlertOpen, closeAlert, alertContent } = context;
 
   return (
     <div className={classes.root}>
-      <Collapse in={open}>
+      <Collapse in={isAlertOpen}>
         <MaterialAlert
           action={
             <IconButton
               aria-label="close"
               color="inherit"
               size="small"
-              onClick={() => {
-                setOpen(false);
-              }}
+              onClick={closeAlert}
             >
               <CloseIcon fontSize="inherit" />
             </IconButton>
           }
         >
-          Close me!
+          {alertContent}
         </MaterialAlert>
       </Collapse>
     </div>
