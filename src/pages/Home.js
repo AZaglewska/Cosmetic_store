7da08ec0b655f2f41f36873238032003753bfc5b
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-01-04 12:35:24 +0100

    deleting a sliderWraper

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,10 @@
 import React from "react";
-import styled from "styled-components";
 import Slider from "../components/slider/Slider";
 
 const Home = () => {
-  const SliderWraper = styled.div`
-    width: 100%;
-    height: 80vh;
-    margin: 0;
-  `;
-
   return (
     <>
-      <SliderWraper>
-        <Slider />
-      </SliderWraper>
-
-      <h1>Home</h1>
+      <Slider />
     </>
   );
 };
