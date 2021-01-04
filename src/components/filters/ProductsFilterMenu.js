16901d882c9b740a45b2d98d6c06e0ca9fc2ddf5
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-01-04 12:58:47 +0100

    adding PriceFilter component and ProductNameFilter component to ProductsFilterMenu

--- src/components/filters/ProductsFilterMenu.js
+++ src/components/filters/ProductsFilterMenu.js
@@ -1,11 +1,15 @@
 import React from "react";
 import CategoryFilter from "./CategoryFilter";
+import PriceFilter from "./PriceFilter";
+import ProductNameFilter from "./ProductNameFilter";
 
 const ProductsFilterMenu = () => {
   return (
     <>
       <h2>Filter menu:</h2>
       <CategoryFilter />
+      <PriceFilter />
+      <ProductNameFilter />
     </>
   );
 };
