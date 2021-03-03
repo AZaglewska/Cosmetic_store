c3adca952b6a0b7b52f28b7bd421ae267ec889a2
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-03-03 12:16:42 +0100

    modifying a styles

--- src/components/filters/FreeDeliveryFilter.js
+++ src/components/filters/FreeDeliveryFilter.js
@@ -1,20 +1,37 @@
 import React, { useContext } from "react";
 import CosmeticStoreContext from "../../context";
+import styled from "styled-components";
+import { FormControlLabel } from "@material-ui/core";
+import { GreenCheckbox } from "./filtersStyles/FreeDeliveryFilterStyles";
+
+const CheckboxWrapper = styled.div`
+  @media (max-width: 990px) {
+    margin-top: 22px;
+  }
+  label {
+    color: ${({ theme }) => theme.colors.smokeyGrey};
+    margin-left: 1px;
+  }
+`;
 
 const FreeDeliveryFilter = () => {
   const context = useContext(CosmeticStoreContext);
   const { freeDelivery, handleFreeDeliveryChange } = context;
 
   return (
-    <div>
+    <CheckboxWrapper>
       <label htmlFor="freeDelivery">free delivery:</label>
-      <input
-        type="checkbox"
-        id="freeDelivery"
-        onChange={handleFreeDeliveryChange}
-        checked={freeDelivery}
+
+      <FormControlLabel
+        control={
+          <GreenCheckbox
+            checked={freeDelivery}
+            onChange={handleFreeDeliveryChange}
+            id="freeDelivery"
+          />
+        }
       />
-    </div>
+    </CheckboxWrapper>
   );
 };
 
