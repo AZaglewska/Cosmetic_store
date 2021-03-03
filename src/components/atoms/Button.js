0ecf24bdd67e6df96590bb17e831b809419b0466
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-03-03 11:44:01 +0100

    adding new Buttons and new features

--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -1,22 +1,106 @@
 import React from "react";
 import styled, { css } from "styled-components";
 import { zoomIn } from "../../GlobalStyles/animations";
+import { ButtonIconSvg } from "../../components/ProductsList/ProductListStyles";
+import { CartIconSvg } from "../../pages/stylesPages/SingleProductStyles";
 
 const StyledButton = styled.button`
   color: white;
-  background-color: ${({ type }) => (type === "second" ? "blue" : "red")};
   padding: 10px 20px;
   outline: none;
   cursor: pointer;
 
-  ${({ type }) =>
-    type === "thrid" &&
+  ${({ styleType }) =>
+    styleType === "pinkShopButton" &&
     css`
-      color: yellow;
-      background-color: black;
+      border: 2px solid ${({ theme }) => theme.colors.pink};
+      background-color: transparent;
+      color: ${({ theme }) => theme.colors.pink};
+      outline: none;
+      cursor: pointer;
+      padding: 20px 40px;
+      font-size: 2rem;
+      transition: ease 1s;
+      border-radius: 5px;
+      margin-top: -15px;
+      &:active {
+        transform: translateY(7px);
+      }
+
+      &:hover {
+        background-color: ${({ theme }) => theme.colors.pink};
+        color: white;
+      }
+      @media (max-width: 413px) {
+        padding: 15px 30px;
+        font-size: 1.5rem;
+      }
+    `}
+
+  ${({ styleType }) =>
+    styleType === "pinkCartButton" &&
+    css`
+      background: ${(props) =>
+        props.form ? " #cea1b6" : "rgba(255, 255, 255, 0.25)"};
+      opacity: 0.8;
+      box-shadow: 6px 7px 19px 2px #dbb7bc;
+      border-radius: 5px;
+      outline: none;
       border: none;
-      border-radius: 12px;
-      padding: 50px;
+      padding: 20px 50px;
+      color: white;
+      font-size: 20px;
+      cursor: pointer;
+      transition: fill 1s;
+      &:active {
+        transform: translateY(1px);
+      }
+
+      &:hover {
+        opacity: 1;
+        color: ${({ theme }) => theme.colors.lightYellow};
+      }
+
+      @media (max-width: 413px) {
+        width: 80%;
+        height: 50px;
+        font-size: 12px;
+      }
+    `}
+
+    ${({ styleType }) =>
+    styleType === "pinkProductsButton" &&
+    css`
+      background: ${({ theme }) => theme.colors.pink};
+      opacity: 0.8;
+      box-shadow: 6px 7px 19px 2px #dbb7bc;
+      border-radius: 5px;
+      outline: none;
+      border: none;
+      padding: ${(props) =>
+        props.singleProduct ? " 20px 30px;" : " 15px 20px;"};
+      color: white;
+      font-size: 18px;
+      margin: 20px 0;
+      cursor: pointer;
+      transition: fill 1s;
+
+      &:active {
+        transform: translateY(1px);
+      }
+
+      &:hover ${ButtonIconSvg} {
+        fill: ${({ theme }) => theme.colors.lightYellow};
+      }
+
+      &:hover ${CartIconSvg} {
+        fill: ${({ theme }) => theme.colors.lightYellow};
+      }
+
+      &:hover {
+        opacity: 1;
+        color: ${({ theme }) => theme.colors.lightYellow};
+      }
     `}
 
   ${({ cartIcon }) =>
@@ -31,8 +115,12 @@ const StyledButton = styled.button`
       color: #000000;
       font-size: 16px;
       animation: ${zoomIn} 0.8s;
-      margin: 0 10px;
+      margin: 0px 10px;
       padding: 23px;
+
+      @media (max-width: 682px) {
+        background-size: 60%;
+      }
     `}
 
     ${({ searchIcon }) =>
@@ -44,15 +132,11 @@ const StyledButton = styled.button`
       border: none;
       background-size: contain;
       padding: 15px;
-      /* background-position: 50% 54%; */
-      /* color: #4ea69e;
-      font-size: 16px; */
-      /* margin-left: auto; */
       animation: ${zoomIn} 0.8s;
+      @media (max-width: 682px) {
+        background-size: 80%;
+      }
     `}
-
- 
-
     ${({ closeIcon }) =>
     closeIcon &&
     css`
@@ -63,26 +147,30 @@ const StyledButton = styled.button`
       background-size: 50%;
       background-position: 59% 64%;
       animation: ${zoomIn} 0.8s;
-      /* color: #4ea69e;
-      font-size: 16px; */
     `}
 `;
 
 const Button = ({
   children,
-  type,
+  styleType,
   onClickFn,
   cartIcon,
   searchIcon,
   closeIcon,
+  buttonType,
+  form,
+  singleProduct,
 }) => {
   return (
     <StyledButton
       onClick={onClickFn}
-      type={type}
+      styleType={styleType}
       cartIcon={cartIcon}
       searchIcon={searchIcon}
       closeIcon={closeIcon}
+      type={buttonType ? buttonType : "button"}
+      form={form}
+      singleProduct={singleProduct}
     >
       {children}
     </StyledButton>
