3a2ad1405d0dc058706ac31c89f7b52e09dea0ab
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-03-03 12:02:15 +0100

    adding a Poper and Footer components to MainTemplate

--- src/templates/MainTemplate.js
+++ src/templates/MainTemplate.js
@@ -4,14 +4,18 @@ import Cart from "../components/Cart/Cart";
 import { ThemeProvider } from "styled-components";
 import { mainTheme } from "../GlobalStyles/mainTheme";
 import Alert from "../components/Alert";
+import Poper from "../components/navigations/Poper";
+import Footer from "../components/navigations/Footer";
 
 const MainTemplate = ({ children }) => {
   return (
     <ThemeProvider theme={mainTheme}>
       <Navbar />
+      <Poper />
       <Alert />
       <Cart />
       {children}
+      <Footer />
     </ThemeProvider>
   );
 };
