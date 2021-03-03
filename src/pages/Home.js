ae00666e2d2f12b2a42d8f33cae023eb2186b565
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-03-03 11:50:43 +0100

    improving a components structures

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Slider from "../components/slider/Slider";
+import DiscountedProductsCarousel from "../components/DiscountedProductsCarousel/DiscountedProductsCarousel";
 
 const Home = () => {
   return (
     <>
       <Slider />
+      <DiscountedProductsCarousel />
     </>
   );
 };
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -2,10 +2,10 @@ import React from "react";
 import ProductsFilterMenu from "../components/filters/ProductsFilterMenu";
 import ProductsList from "../components/ProductsList/ProductsList";
 import styled from "styled-components";
-import Background from "../assets/navbarImages/10.jpg";
+import navbarImage from "../assets/navbarImages/navbarImage2.jpg";
 
 const StyledWrapper = styled.div`
-  background-image: url(${(props) => props.bg});
+  background-image: url(${(props) => props.navbarImage});
   height: 300px;
   width: 100%;
   background-repeat: no-repeat;
@@ -19,14 +19,26 @@ const StyledWrapper = styled.div`
   font-size: 35px;
 `;
 
+const ProductWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  color: ${({ theme }) => theme.colors.smokeyGrey};
+  h1 {
+    margin-bottom: 40px;
+  }
+`;
 const Products = () => {
   return (
     <>
-      <StyledWrapper bg={Background}></StyledWrapper>
-      <h1>Products</h1>
-      <ProductsFilterMenu />
-
-      <ProductsList />
+      <StyledWrapper navbarImage={navbarImage}></StyledWrapper>
+      <ProductWrapper>
+        <ProductsFilterMenu />
+        <h1>Our Products</h1>
+        <ProductsList />
+      </ProductWrapper>
     </>
   );
 };
