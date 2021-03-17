baadb2627a2600b75b0b2b171ca30d49e8705ff4
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-03-17 13:07:55 +0100

    adding a BrowserRouter and ProductsContextProvider to index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Root from "./Root";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import ProductsContextProvider from "./context/ProductsContextProvider";
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <ProductsContextProvider>
+      <Root />
+    </ProductsContextProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
